5caafb27f199acf380c6016301daeefe61c347b0
Sadriddin Mamatov <sadriddin1005@gmail.com>
2024-10-23 22:08:08 +0500

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test task </h1>
+      <p>taskni vazifasi githubdagi repositoriyni collabratorlar bilgan manipulatsiya qilish va changelarni
+        merge qilgandan so'ng UI da updated versiyasini ko'rish</p>
+    </div>
+  );
+}
+
+export default App;
