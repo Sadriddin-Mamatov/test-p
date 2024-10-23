ac9647d5d0c80709a600f29480fbf3aecf56e672
Sadriddin Mamatov <sadriddin1005@gmail.com>
2024-10-23 22:49:02 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,8 @@ function App() {
     <div className="App">
       <h1>Test task </h1>
       <p>taskni vazifasi githubdagi repositoriyni collabratorlar bilgan manipulatsiya qilish va changelarni
-        merge qilgandan so'ng UI da updated versiyasini ko'rish</p>
-        <div>
-            men change qildim bu yerda
-        </div>
+        merge qilgandan so'ng UI da updated versiyasini ko'rish
+      </p>
     </div>
   );
 }
